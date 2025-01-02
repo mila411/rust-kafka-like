1e39024f3bada4d46d32a6c205ce03fed887a569
AndreVale69 <sforzini31@gmail.com>
2025-01-02 02:02:21 +0100

    docs(web_console): update main
    Refs: #13

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+// TODO: module description
+#![warn(missing_docs)]
 mod web_console;
 
+/// The main entry point for the Pilgrimage application.
+///
+/// This function creates and initializes a new HTTP server
+/// to provide various commands to the user (`/start`, `/stop`, `/status`, etc.).
+/// For more details about the HTTP server, see [`web_console::run_server`].
+///
+/// The application also uses the [Tokio runtime](https://tokio.rs/),
+/// which provides asynchronous support for Rust,
+/// enabling the development of high performance network applications.
+///
+/// # Returns
+///
+/// Returns a `std::io::Result<()>` indicating the success or failure of the
+/// web server execution.
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     web_console::run_server().await
